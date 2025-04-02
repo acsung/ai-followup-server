24a64de243c349073c6e8ee067cc30b4cd9ad1c1
acsung <145598506+acsung@users.noreply.github.com>
2025-04-02 13:48:08 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
+const allowedOrigins = ['https://newhomeknowitall.com'];
+
+app.use(cors({
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  }
+}));
 
